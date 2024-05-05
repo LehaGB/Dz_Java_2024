d5960c7f2c118236fe8464b2344c1b9848f5d8a7
Алексей Шербаков <leha.gb.2023@mail.ru>
2024-05-05 19:33:59 +1000

    fixed

--- src/dz_seminar_4/Main_seminar_4.java
+++ src/dz_seminar_4/Main_seminar_4.java
@@ -1,27 +1,35 @@
 package dz_seminar_4;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Main_seminar_4 {
     public static void main(String[] args) {
-        Task_2<Integer> integerTask2 = new Task_2<>();
-        if(args.length == 0){
-            integerTask2.enqueue(1);
-            integerTask2.enqueue(10);
-            integerTask2.enqueue(15);
-            integerTask2.enqueue(5);
+        Integer[] arr = {9, 5, 1, 0, 3, 8, 3, 2};
+
+        System.out.println(SortArray(arr));
+    }
+    public static Integer[] SortArray(Integer[] array){
+        ArrayList<Integer> result = new ArrayList<>();
+        ArrayList<Integer> temp = new ArrayList<>();
+        Integer left;
+        int[] right;
+        int midlle = array.length / 2;
+        for (int i = 0; i <= midlle; i++) {
+            if(array[i] < midlle){
+                result.add(array[i]);
+                for (int j = 0; j <= result.size() - 1; j++) {
+                    if(array[i] > result.get(j)){
+                       int arr =  array[i];
+                        array[i] = result.get(j);
+//                        result.get(j) = arr;
+                    }
+                }
+            }
+//            else if(array[i] > midlle){
+//                result.add(right = array[i]);
+//            }
         }
-        else {
-            integerTask2.enqueue(Integer.parseInt(args[0]));
-            integerTask2.enqueue(Integer.parseInt(args[1]));
-            integerTask2.enqueue(Integer.parseInt(args[2]));
-            integerTask2.enqueue(Integer.parseInt(args[3]));
-        }
-
-        System.out.println(integerTask2.getElements());
-
-        integerTask2.dequeue();
-        integerTask2.dequeue();
-        System.out.println(integerTask2.getElements());
-
-        System.out.println(integerTask2.first());
+        return array;
     }
 }
